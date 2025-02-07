b58a1c955b42078ed8c04e99d5b16eb9c91c99d3
Daz <shoeshinesamurai@gmail.com>
2025-02-06 21:10:03 -0800

    typescript function types exercise

--- dist/ex05.js
+++ dist/ex05.js
@@ -1,9 +1,8 @@
+"use strict";
 // Create a function called sumAllNumbers.
 // This function should accept any number of numbers using a rest parameter.
 // It should return the sum of all the numbers passed to it.
-
-function sumAllNumbers(...nums: number[]): number {
-  return nums.reduce((acc, crr) => acc + crr, 0)
+function sumAllNumbers(...nums) {
+    return nums.reduce((acc, crr) => acc + crr, 0);
 }
-
 console.log(sumAllNumbers(1, 2, 3, 4, 5)); // Expected output: 15
